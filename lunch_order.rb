4d006ae635d3461ba942c54f260ce9483d1b7062
JoshSutcliffe <joshclement12@gmail.com>
2016-02-28 20:34:22 +1100

    lunch order imporvement

--- lunch_order.rb
+++ lunch_order.rb
@@ -4,30 +4,34 @@ p "Hello to Dirty Joes, Melbourne's best finger food. How many of you are dining
 diners = gets.chomp.to_i
 
 count = 0
-customers = []
-orders = []
+@customers = []
+@orders = []
 
 while count < diners do
   p "What is your name?"
-  customers << gets.chomp
+  @customers << gets.chomp
   p "And what are you having?"
-  orders << gets.chomp
+  @orders << gets.chomp
   p "Is that all bruv? y/n"
   answer = gets.upcase.chomp
   if answer == "Y"
     p "Thanks for the order!"
   elsif answer == "N"
     p "What else would you like?"
-    orders << gets.chomp
+    @orders << gets.chomp
   else
     p "Invalid response, you don't deserve to order more"
   end
   count += 1 
 end
 
-# for customers do |x|
-#   i = 0
-#   p "#{x} has ordered #{orders[i]}"
-#   i += 1
-#   end
-# end
+def print_order
+  @customers.each do |x|
+    i = 0
+    p "#{x} has ordered #{@orders[i]}"
+    i += i
+  end
+  p "Thanks for coming to Dirty Joes!"
+end
+
+print_order
